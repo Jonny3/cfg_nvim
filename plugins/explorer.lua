76ae51b5c91635ea8fb8d6bce57d8a9aefbd306f
jonas <jonas.bleich@gmx.de>
2025-03-30 12:28:37 +0200

    updated nvim configuration

--- plugins/explorer.lua
+++ plugins/explorer.lua
@@ -12,19 +12,19 @@ return {
     {
       '<leader>fe',
       function()
-        require('neo-tree.command').execute { toggle = true, dir = vim.fn.getcwd() }
+        require('neo-tree.command').execute { toggle = true, dir = vim.g.initial_cwd }
       end,
       desc = 'Explorer NeoTree (Root Dir)',
     },
-    -- {
-    --   '<leader>fE',
-    --   function()
-    --     require('neo-tree.command').execute { toggle = true, dir = vim.uv.cwd() }
-    --   end,
-    --   desc = 'Explorer NeoTree (cwd)',
-    -- },
+    {
+      '<leader>fE',
+      function()
+        require('neo-tree.command').execute { toggle = true, dir = vim.fn.expand '%:p:h' }
+      end,
+      desc = 'Explorer NeoTree (cwd)',
+    },
     { '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (Root Dir)', remap = true },
-    -- { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
+    { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
     {
       '<leader>ge',
       function()
