1785dbdf36e7a8a84a4f74632f29eacc8f816aab
jonas <jonas.bleich@gmx.de>
2025-05-23 17:16:58 +0200

    make transparent

--- init.lua
+++ init.lua
@@ -802,7 +802,10 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
+          sidebars = 'transparent',
+          floats = 'transparend',
           comments = { italic = false }, -- Disable italics in comments
         },
       }
