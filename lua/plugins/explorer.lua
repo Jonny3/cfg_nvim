c6c6a2dbe0016e8bc02ea38c5f5ae739b754c4ed
jonas <jonas.bleich@gmx.de>
2025-06-03 10:14:54 +0200

    jonas: move treesitter to right size

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -7,6 +7,11 @@ return {
     'MunifTanjim/nui.nvim',
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  opts = {
+    window = {
+      position = 'right',
+    },
+  },
   cmd = 'Neotree',
   keys = {
     {
