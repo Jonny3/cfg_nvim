c5e4f4593c9b6ff2b2f4f0d9fc8f60e0c8c92420
jonas <jonas.bleich@gmx.de>
2025-06-24 18:17:39 +0200

    jonas: add vim + copymatches

--- init.lua
+++ init.lua
@@ -123,6 +123,9 @@ vim.opt.termguicolors = true
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+--
+-- source .vim files for additional commands
+vim.cmd 'source ~/.config/nvim/vim/copymatch.vim'
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
